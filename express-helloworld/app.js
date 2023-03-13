6097214958e1796acace6fd5e664ca242fb77edb
roadcyclist445 <rje532@gmail.com>
2023-03-12 22:33:18 -0400

    added console logging

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,10 +2,12 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
+  console.log('Get');
   res.send('Hello World!\n');
 });
 
 app.get('/mars', function(req, res) {
+  console.log('Get mars');
   res.send('Hello Mars!\n');
 })
 
